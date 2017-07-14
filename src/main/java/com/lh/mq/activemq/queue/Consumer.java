6bc5f6f06ae63e936763fdf3a97e78af25fc0b52
lhxyq <2446059046@qq.com>
2017-07-14 22:19:19 +0800

    activeMQ's demo

---
+++ src/main/java/com/lh/mq/activemq/queue/Consumer.java
@@ -0,0 +1,39 @@
+package com.lh.mq.activemq.queue;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+/**
+ * Created by LH 2446059046@qq.com on 2017/7/14.
+ */
+public class Consumer {
+    private static final String url = "tcp://localhost:61616";
+    private static final String queueName = "queue-test";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ActiveMQConnectionFactory(url);
+
+        Connection connection = factory.createConnection();
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        Destination destination = session.createQueue(queueName);
+
+        MessageConsumer consumer = session.createConsumer(destination);
+
+        consumer.setMessageListener(new MessageListener() {
+            @Override
+            public void onMessage(Message message) {
+                TextMessage textMessage = (TextMessage) message;
+                try {
+                    System.out.println("接收消息" + textMessage.getText());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    //connection.close();
+}
---
+++ src/main/java/com/lh/mq/activemq/topic/Producer.java
@@ -0,0 +1,34 @@
+package com.lh.mq.activemq.topic;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+/**
+ * Created by LH 2446059046@qq.com on 2017/7/14.
+ */
+public class Producer {
+    private static final String url = "tcp://localhost:61616";
+    private static final String topicName = "topic-test";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ActiveMQConnectionFactory(url);
+
+        Connection connection = factory.createConnection();
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        Destination destination = session.createTopic(topicName);
+
+        MessageProducer producer = session.createProducer(destination);
+
+        for (int i = 0; i < 100; i++) {
+            TextMessage textMessage = session.createTextMessage("test" + i);
+            producer.send(textMessage);
+            System.out.println("发送消息" + textMessage.getText());
+        }
+
+        connection.close();
+    }
+}
